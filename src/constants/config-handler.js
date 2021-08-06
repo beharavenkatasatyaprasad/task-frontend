cd3d86bc0e909d7ba9d6cb8077ac84a26817e82d
beharavenkatasatyaprasad <satyaprasadbehara@gmail.com>
2021-08-06 23:15:27 +0530

    included configurations and api requests module

---
+++ src/constants/config-handler.js
@@ -0,0 +1,8 @@
+import Config from "./config.json";
+
+export function getConfig() {
+  let env = process.env.REACT_APP_NODE_ENV
+    ? process.env.REACT_APP_NODE_ENV
+    : "common";
+  return Config[env];
+}
