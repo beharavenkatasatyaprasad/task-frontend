7e1d39e2102dba1ecb99847e12fee42b2b60fb0b
beharavenkatasatyaprasad <satyaprasadbehara@gmail.com>
2021-08-06 23:34:23 +0530

    Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import { AwesomeButton } from "react-awesome-button";
+
+const Navbar = () => {
+  const [scrollHeaderClass, setscrollHeaderClass] = useState("top");
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      let scrollHeaderClass = "bg-vey shadow";
+      if (window.scrollY === 0) {
+        scrollHeaderClass = "";
+      }
+      setscrollHeaderClass(scrollHeaderClass);
+    });
+  });
+
+  return (
+    <>
+      <header
+        id="header_sec"
+        className={`header_sec bg_transparent border_none ${scrollHeaderClass}`}
+      >
+        <div className="container-fluid p-3">
+          <div className="row">
+            <div className="col-12">
+              <div className="header_box">
+                <div className="d-flex justify-content-between">
+                  <p className={`header_logo my-auto`}>TEST APP</p>
+                  <div className="my-auto">
+                    <AwesomeButton type="secondary" className="mx-2">
+                      Add Agent
+                    </AwesomeButton>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Navbar;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-date-range/dist/styles.css';
+import 'react-date-range/dist/theme/default.css';
+import 'react-awesome-button/dist/styles.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
